7a5cd65715becf916c60881f52934ee21652202f
thisisartorius <artur.poe@gmail.com>
2021-12-18 17:41:24 -0500

    Update Card.js

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -13,7 +13,7 @@ const Card = ({
     // Определяем, являемся ли мы владельцем текущей карточки
     const isOwn = card.owner === currentUser._id;
     // Определяем, есть ли у карточки лайк, поставленный текущим пользователем
-    const isLiked = card.likes.some(i => i._id === currentUser._id);
+    const isLiked = card.likes.some(i => i === currentUser._id);
 
     const handleDeleteSubmit = (e) => {
         e.preventDefault();
